0038d1c6c0e453da3a05e946fd532f683d329c0e
Garule Prabhudas <garule.prabhudas@aepl.com>
2017-04-25 20:00:50 +0530

    delete alternate nodes in linked list

---
+++ LinkedList/10.cs
@@ -0,0 +1,67 @@
+using System;
+
+class Node
+{
+    public int data;
+    public Node next;
+    public Node(int data)
+    {
+        this.data = data;
+        next = null;
+    }
+}
+class LinkedList
+{
+    Node head;
+    public void AddNode(int data)
+    {
+        Node temp = new Node(data);
+        if(head == null)
+        {
+            head = temp;
+            return;
+        }
+        Node ptr  = head;
+        while(ptr.next != null)
+            ptr = ptr.next;
+        ptr.next = temp;
+    }
+    public void PrintList()
+    {
+        Node temp = head;
+        while(temp != null)
+        {
+            System.Console.Write(temp.data +" ");
+            temp = temp.next;
+        }
+        System.Console.WriteLine();
+    }
+    public  void DeleteAlternateNodes()
+    {
+        Node temp = head;
+        while(temp != null && temp.next != null)
+        {
+            Node ptr = temp.next.next;
+            temp.next = ptr;
+            temp = ptr;
+        }
+    }
+}
+class MainClass
+{
+    public static void Main(string [] args)
+    {
+        LinkedList list  = new LinkedList();
+        list.AddNode(1);
+        list.AddNode(2);
+        list.AddNode(3);
+        list.AddNode(4);
+        list.AddNode(5);
+        list.AddNode(6);
+        list.AddNode(7);
+        list.PrintList();
+        list.DeleteAlternateNodes();
+        System.Console.WriteLine(": After Deleting Alternate Nodes :");
+        list.PrintList();
+    }
+}
